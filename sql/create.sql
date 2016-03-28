09a8785ae871d0e607c907c4b235edbf6847d0bf
Paavo Huhtala <paavo.huhtala@gmail.com>
2016-03-28 18:21:14 +0300

    Add initial db schema and test data

---
+++ sql/create.sql
@@ -0,0 +1,97 @@
+﻿
+CREATE TYPE user_role AS ENUM (
+	'unverifiedUser',
+	'user',
+	'unapprovedSeller',
+	'seller',
+	'admin');
+
+CREATE TABLE Addresses (
+	id SERIAL PRIMARY KEY NOT NULL,
+	street1 TEXT NOT NULL,
+	street2 TEXT,
+	postalCode TEXT NOT NULL,
+	city TEXT NOT NULL,
+	country CHAR(2) NOT NULL DEFAULT('FI')
+);
+
+CREATE TABLE Users (
+	id SERIAL PRIMARY KEY NOT NULL,
+	role user_role NOT NULL DEFAULT('unverifiedUser'),
+	name TEXT NOT NULL,
+	email VARCHAR(255) NOT NULL, -- IETF maximum for email address length
+	password VARCHAR(60) NOT NULL, -- We're storing bcrypt hashes, which include the salt
+	phone TEXT NOT NULL,
+	address INTEGER REFERENCES Addresses(id) NOT NULL
+);
+
+CREATE TABLE Sellers (
+	userId INTEGER REFERENCES Users(id) NOT NULL,
+	companyName TEXT NOT NULL,
+	billingAddress INTEGER REFERENCES Addresses(id),
+	vatId TEXT NOT NULL,
+	PRIMARY KEY (userId)
+);
+
+CREATE TABLE Amenities (
+	id SERIAL PRIMARY KEY NOT NULL,
+	name TEXT NOT NULL,
+	icon TEXT
+);
+
+CREATE TABLE Lodgings (
+	id SERIAL PRIMARY KEY NOT NULL,
+	owner INTEGER REFERENCES Sellers(userId) NOT NULL,
+	address INTEGER REFERENCES Addresses(id) NOT NULL,
+	name TEXT NOT NULL,
+	description TEXT NOT NULL,
+	isPublic BOOLEAN NOT NULL DEFAULT(FALSE),
+	reservationStart TIME NOT NULL,
+	reservationEnd TIME NOT NULL,
+	pricePerNight MONEY CHECK (pricePerNight > 0::money) NOT NULL,
+	CHECK(reservationStart >= reservationEnd),
+	area NUMERIC CHECK (area > 0),
+	floors INTEGER CHECK (floors >= 1),
+	builtYear INTEGER,
+	renovatedYear INTEGER, 
+	CHECK (renovatedYear >= builtYear)
+);
+
+CREATE TABLE LodgingAmenities (
+	lodging INTEGER REFERENCES Lodgings(id) NOT NULL,
+	amenity INTEGER REFERENCES Amenities(id) NOT NULL,
+	PRIMARY KEY (lodging, amenity)
+);
+
+CREATE TYPE reservation_type AS ENUM ('external', 'user');
+
+CREATE TABLE Reservations (
+	id SERIAL PRIMARY KEY NOT NULL,
+	lodging INTEGER REFERENCES Lodgings(id) NOT NULL,
+	type reservation_type NOT NULL DEFAULT('user'),
+	begins TIMESTAMP NOT NULL,
+	ends TIMESTAMP NOT NULL,
+	CHECK (ends > begins)
+);
+
+CREATE TABLE ExternalReservations (
+	reservation INTEGER REFERENCES Reservations(id) NOT NULL,
+	reason TEXT NOT NULL,
+	PRIMARY KEY (reservation)
+);
+
+CREATE TABLE UserReservations (
+	reservation INTEGER REFERENCES Reservations(id) NOT NULL,
+	customer INTEGER REFERENCES Users(id) NOT NULL,
+	price MONEY NOT NULL,
+	PRIMARY KEY (customer, reservation)
+);
+
+CREATE TABLE Reviews (
+	id SERIAL PRIMARY KEY NOT NULL,
+	isPublic BOOLEAN NOT NULL DEFAULT(TRUE),
+	lodging INTEGER REFERENCES Lodgings(id) NOT NULL,
+	author INTEGER REFERENCES Users(id) NOT NULL,
+	content TEXT NOT NULL,
+	rating INTEGER CHECK (rating >= 1 AND rating <= 5)
+);
---
+++ sql/test_data.sql
@@ -0,0 +1,54 @@
+
+INSERT INTO Addresses (street1, postalCode, city) VALUES
+	('Demonipolku 6', '00666', 'Kerava'),
+	('Low Earth Orbit', '01337', 'Space'),
+	('Placeholderin Katu N', '0101010', 'Undefinedin kaupunki'),
+	('Placeholderin Katu N + 1', '0101010', 'Undefinedin kaupunki');
+
+INSERT INTO Addresses (street1, street2, postalCode, city) VALUES
+	('Leppäsuonkatu 11', 'Christina Regina', '00100', 'Helsinki');
+
+INSERT INTO Users (role, name, email, password, phone, address) VALUES
+	('user', 'Urho Karhu', 'urho.karhu@domus-gaudium.fi',
+	'$2a$10$JGbFjNhhRteZjXy0KF9EE.VODp.NGNuJeFcLMvYNUxTzTcClpRTjS', -- 'safedefaults'
+	'0700-BMUR-KLUSTERPONG', 5),
+	('admin', 'The Overseer', 'that.guy@vault-tec.us',
+	'$2a$09$K/wkfFVfrUKv36HszesO1e6P/VKPmq/7qQaYABWOlWz1btASFm1Re', -- 'please stand by',
+	'1-776-LIBERTY', 2),
+	('seller', 'Rich Cabin', 'rich.cabin@cabinsfortherich.com',
+	'$2a$09$AhXEnLew4nIZ6lTu.uSVLOT1MUNH0dDro4KGTu1zt/ntk4RKd.IYK', -- 'bcrypt is a key derivation function'
+	'123456', 3);
+
+INSERT INTO Sellers (userId, companyName, vatId) VALUES
+	(3, 'Rich Cabin''s Cabins for the rich', 'FI12345678');
+
+INSERT INTO Amenities (name) VALUES
+	('Sauna'),
+	('Tiskikone'),
+	('Oma ranta'),
+	('Laituri'),
+	('Venevarasto'),
+	('LCD-televisio'),
+	('Lemmikit sallittuja');
+
+INSERT INTO Lodgings (owner, address, name, description, isPublic, reservationStart, reservationEnd, pricePerNight) VALUES
+	(3, 2, 'Kansainvälinen avaruusasema',
+	'1990-luvun lopulla rakennettu ja useaan otteeseen laajennettu jännittävä kohde matallalla maan kiertoradalla!',
+	TRUE, '12:00:00', '10:00:00', 10000);
+
+INSERT INTO LodgingAmenities (lodging, amenity) VALUES
+	(1, 6),
+	(1, 1);
+
+INSERT INTO Reservations (lodging, type, begins, ends) VALUES
+	(1, 'external', '2016-01-01', '2016-06-01 06:00:00'),
+	(1, 'user', '2016-06-01 12:00:00', '2016-06-04 11:00:00');
+
+INSERT INTO ExternalReservations (reservation, reason) VALUES
+	(1, 'Remontissa.');
+
+INSERT INTO UserReservations (reservation, customer, price) VALUES
+	(2, 1, 440);
+
+INSERT INTO Reviews (lodging, author, content, rating) VALUES
+	(1, 3, '5/5 proper great', 5);
