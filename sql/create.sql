df0696b0509483ff3bf63d7a5731a6f7c4e4cfab
Paavo Huhtala <paavo.huhtala@gmail.com>
2016-04-16 20:47:55 +0300

    Use quoted table names

--- sql/create.sql
+++ sql/create.sql
@@ -1,14 +1,14 @@
 ﻿
 CREATE EXTENSION btree_gist;
 
-CREATE TYPE user_role AS ENUM (
+CREATE TYPE "user_role" AS ENUM (
 	'unverifiedUser',
 	'user',
 	'unapprovedSeller',
 	'seller',
 	'admin');
 
-CREATE TABLE Addresses (
+CREATE TABLE "Addresses" (
 	id SERIAL PRIMARY KEY NOT NULL,
 	street1 TEXT NOT NULL,
 	street2 TEXT,
@@ -17,42 +17,42 @@ CREATE TABLE Addresses (
 	country CHAR(2) NOT NULL DEFAULT('FI')
 );
 
-CREATE TABLE Users (
+CREATE TABLE "Users" (
 	id SERIAL PRIMARY KEY NOT NULL,
 	role user_role NOT NULL DEFAULT('unverifiedUser'),
 	name TEXT NOT NULL,
 	email VARCHAR(255) NOT NULL, -- IETF maximum for email address length
 	password VARCHAR(60) NOT NULL, -- We're storing bcrypt hashes, which include the salt
 	phone TEXT NOT NULL,
-	address INTEGER REFERENCES Addresses(id) NOT NULL
+	address INTEGER REFERENCES "Addresses"(id) NOT NULL
 );
 
-CREATE TABLE Sessions (
-	user_id INTEGER REFERENCES Users(id) NOT NULL,
+CREATE TABLE "Sessions" (
+	user_id INTEGER REFERENCES "Users"(id) NOT NULL,
 	created TIMESTAMP NOT NULL DEFAULT(now()::timestamp),
 	valid_until TIMESTAMP NOT NULL DEFAULT(now()::timestamp + interval '14 days'),
 	token CHAR(32) NOT NULL UNIQUE,
 	PRIMARY KEY (user_id)
 );
 
-CREATE TABLE Sellers (
-	user_id INTEGER REFERENCES Users(id) NOT NULL,
+CREATE TABLE "Sellers" (
+	user_id INTEGER REFERENCES "Users"(id) NOT NULL,
 	company_name TEXT NOT NULL,
-	billing_address INTEGER REFERENCES Addresses(id),
+	billing_address INTEGER REFERENCES "Addresses"(id),
 	vatId TEXT NOT NULL,
 	PRIMARY KEY (user_id)
 );
 
-CREATE TABLE Amenities (
+CREATE TABLE "Amenities" (
 	id SERIAL PRIMARY KEY NOT NULL,
 	name TEXT NOT NULL,
 	icon TEXT
 );
 
-CREATE TABLE Lodgings (
+CREATE TABLE "Lodgings" (
 	id SERIAL PRIMARY KEY NOT NULL,
-	owner INTEGER REFERENCES Sellers(user_id) NOT NULL,
-	address INTEGER REFERENCES Addresses(id) NOT NULL,
+	owner INTEGER REFERENCES "Sellers"(user_id) NOT NULL,
+	address INTEGER REFERENCES "Addresses"(id) NOT NULL,
 	name TEXT NOT NULL,
 	description TEXT NOT NULL,
 	is_public BOOLEAN NOT NULL DEFAULT(FALSE),
@@ -67,45 +67,45 @@ CREATE TABLE Lodgings (
 	CHECK (renovated_year >= built_year)
 );
 
-CREATE TABLE LodgingAmenities (
-	lodging INTEGER REFERENCES Lodgings(id) NOT NULL,
-	amenity INTEGER REFERENCES Amenities(id) NOT NULL,
+CREATE TABLE "LodgingAmenities" (
+	lodging INTEGER REFERENCES "Lodgings"(id) NOT NULL,
+	amenity INTEGER REFERENCES "Amenities"(id) NOT NULL,
 	PRIMARY KEY (lodging, amenity)
 );
 
-CREATE TYPE reservation_type AS ENUM ('external', 'user');
+CREATE TYPE "reservation_type" AS ENUM ('external', 'user');
 
-CREATE TABLE Reservations (
+CREATE TABLE "Reservations" (
 	id SERIAL PRIMARY KEY NOT NULL,
-	lodging INTEGER REFERENCES Lodgings(id) NOT NULL,
+	lodging INTEGER REFERENCES "Lodgings"(id) NOT NULL,
 	type reservation_type NOT NULL DEFAULT('user'),
 	during tsrange NOT NULL,
 	EXCLUDE USING gist (lodging WITH =, during WITH &&)
 );
 
-CREATE TABLE ExternalReservations (
-	reservation INTEGER REFERENCES Reservations(id) NOT NULL,
+CREATE TABLE "ExternalReservations" (
+	reservation INTEGER REFERENCES "Reservations"(id) NOT NULL,
 	reason TEXT NOT NULL,
 	PRIMARY KEY (reservation)
 );
 
-CREATE TABLE UserReservations (
-	reservation INTEGER REFERENCES Reservations(id) NOT NULL,
-	customer INTEGER REFERENCES Users(id) NOT NULL,
+CREATE TABLE "UserReservations" (
+	reservation INTEGER REFERENCES "Reservations"(id) NOT NULL,
+	customer INTEGER REFERENCES "Users"(id) NOT NULL,
 	price MONEY NOT NULL,
 	PRIMARY KEY (customer, reservation)
 );
 
-CREATE TABLE Reviews (
+CREATE TABLE "Reviews" (
 	id SERIAL PRIMARY KEY NOT NULL,
 	is_public BOOLEAN NOT NULL DEFAULT(TRUE),
-	lodging INTEGER REFERENCES Lodgings(id) NOT NULL,
-	author INTEGER REFERENCES Users(id) NOT NULL,
+	lodging INTEGER REFERENCES "Lodgings"(id) NOT NULL,
+	author INTEGER REFERENCES "Users"(id) NOT NULL,
 	content TEXT NOT NULL,
 	rating INTEGER CHECK (rating >= 1 AND rating <= 5)
 );
 
-CREATE VIEW Locations AS (SELECT DISTINCT city FROM Addresses);
+CREATE VIEW "Locations" AS (SELECT DISTINCT city FROM "Addresses");
 
-CREATE INDEX city_index ON Addresses(lower(city));
-CREATE INDEX session_token_index ON Sessions(token);
+CREATE INDEX "city_index" ON "Addresses"(lower(city));
+CREATE INDEX "session_token_index" ON "Sessions"(token);
--- sql/test_data.sql
+++ sql/test_data.sql
@@ -1,14 +1,14 @@
 
-INSERT INTO Addresses (street1, postal_code, city) VALUES
+INSERT INTO "Addresses" (street1, postal_code, city) VALUES
 	('Demonipolku 6', '00666', 'Kerava'),
 	('Low Earth Orbit', '01337', 'Space'),
 	('Placeholderin Katu N', '0101010', 'Undefinedin kaupunki'),
 	('Placeholderin Katu N + 1', '0101010', 'Undefinedin kaupunki');
 
-INSERT INTO Addresses (street1, street2, postal_code, city) VALUES
+INSERT INTO "Addresses" (street1, street2, postal_code, city) VALUES
 	('Leppäsuonkatu 11', 'Christina Regina', '00100', 'Helsinki');
 
-INSERT INTO Users (role, name, email, password, phone, address) VALUES
+INSERT INTO "Users" (role, name, email, password, phone, address) VALUES
 	('user', 'Urho Karhu', 'urho.karhu@domus-gaudium.fi',
 	'$2a$10$JGbFjNhhRteZjXy0KF9EE.VODp.NGNuJeFcLMvYNUxTzTcClpRTjS', -- 'safedefaults'
 	'0700-BMUR-KLUSTERPONG', 5),
@@ -19,10 +19,10 @@ INSERT INTO Users (role, name, email, password, phone, address) VALUES
 	'$2a$09$AhXEnLew4nIZ6lTu.uSVLOT1MUNH0dDro4KGTu1zt/ntk4RKd.IYK', -- 'bcrypt is a key derivation function'
 	'123456', 3);
 
-INSERT INTO Sellers (user_id, company_name, vatId) VALUES
+INSERT INTO "Sellers" (user_id, company_name, vatId) VALUES
 	(3, 'Rich Cabin''s Cabins for the rich', 'FI12345678');
 
-INSERT INTO Amenities (name) VALUES
+INSERT INTO "Amenities" (name) VALUES
 	('Sauna'),
 	('Tiskikone'),
 	('Oma ranta'),
@@ -31,24 +31,24 @@ INSERT INTO Amenities (name) VALUES
 	('LCD-televisio'),
 	('Lemmikit sallittuja');
 
-INSERT INTO Lodgings (owner, address, name, description, is_public, reservation_start, reservation_end, price_per_night) VALUES
+INSERT INTO "Lodgings" (owner, address, name, description, is_public, reservation_start, reservation_end, price_per_night) VALUES
 	(3, 2, 'Kansainvälinen avaruusasema',
 	'1990-luvun lopulla rakennettu ja useaan otteeseen laajennettu jännittävä kohde matallalla maan kiertoradalla!',
 	TRUE, '12:00:00', '10:00:00', 10000);
 
-INSERT INTO LodgingAmenities (lodging, amenity) VALUES
+INSERT INTO "LodgingAmenities" (lodging, amenity) VALUES
 	(1, 6),
 	(1, 1);
 
-INSERT INTO Reservations (lodging, type, during) VALUES
+INSERT INTO "Reservations" (lodging, type, during) VALUES
 	(1, 'external', '[2016-01-01, 2016-06-01 06:00:00]'),
 	(1, 'user', '[2016-06-01 12:00:00, 2016-06-04 11:00:00]');
 
-INSERT INTO ExternalReservations (reservation, reason) VALUES
+INSERT INTO "ExternalReservations" (reservation, reason) VALUES
 	(1, 'Remontissa.');
 
-INSERT INTO UserReservations (reservation, customer, price) VALUES
+INSERT INTO "UserReservations" (reservation, customer, price) VALUES
 	(2, 1, 440);
 
-INSERT INTO Reviews (lodging, author, content, rating) VALUES
+INSERT INTO "Reviews" (lodging, author, content, rating) VALUES
 	(1, 3, '5/5 proper great', 5);
