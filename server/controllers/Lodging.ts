dffd7b1ecb629b8ba427a9b0de82ec86b8dfb4a4
Paavo Huhtala <paavo.huhtala@gmail.com>
2016-04-18 22:00:18 +0300

    Minor changes to views

--- server/controllers/Lodging.ts
+++ server/controllers/Lodging.ts
@@ -3,19 +3,21 @@ import {Request, Response} from "express"
 import {IController} from "../IController"
 import {LodgingDao} from "../database/daos/LodgingDao"
 import {AddressDao} from "../database/daos/AddressDao"
+import {UserDao} from "../database/daos/UserDao"
 
 async function getViewData(id: number) {
-    const lodging = await new LodgingDao().getById(id)
-    const address = await new AddressDao().getById(lodging.address)
+    const lodging = await new LodgingDao().getById(id);
+    const address = await new AddressDao().getById(lodging.address);
+    const owner = await new UserDao().getById(lodging.owner);
     
-    return {lodging: lodging, address: address}
+    return {lodging: lodging, address: address, owner: owner}
 }
 
 export const Lodging : IController = {
-    get: (req: Request, res: Response) => {
+    get: async (req: Request, res: Response) => {
         let id = parseInt(req.params.id, 10);
-        getViewData(id)
-        .then(data => res.render("lodging", data), err => res.send(500, err));             
+        let viewData = await getViewData(id);
+        res.render("lodging", viewData);
     }
 }
 
--- server/controllers/Lodgings.ts
+++ server/controllers/Lodgings.ts
@@ -5,13 +5,16 @@ import {LodgingDao} from "../database/daos/LodgingDao"
 import {getClient} from "../database/Connection"
 import * as pgp from "pg-promise"
 
-const query = `SELECT l.*, a.city FROM "Lodgings" l JOIN "Addresses" a ON l.address = a.id`;
+const query = `
+    SELECT l.*, a.city, u.name AS owner_name FROM "Lodgings" l
+    JOIN "Addresses" a ON l.address = a.id
+    JOIN "Users" u ON  u.id = l.owner`;
 
 export const Lodgings : IController = {
-    get: (req: Request, res: Response) => {
-        getClient().manyOrNone(query).then(rows => {
-            res.render("lodgings", {lodgings: rows});            
-        });
+    get: async (req: Request, res: Response) => {
+        let lodgings = await getClient().manyOrNone(query);
+        
+        res.render("lodgings", {lodgings: lodgings}); 
     }
 }
 
