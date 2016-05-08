75d365c5a9aedb1ccac6431036726cfc8b52d971
Paavo Huhtala <paavo.huhtala@gmail.com>
2016-05-08 14:26:02 +0300

    Minor changes to seller application

--- client/js/controllers/seller_application.ts
+++ client/js/controllers/seller_application.ts
@@ -14,7 +14,7 @@ namespace lodgist.controllers {
             
             $scope.onSend = () => {
                 $http.post("/api/v1/seller_application", $scope.seller).then(succ => {
-                    location.pathname = "/me"
+                    location.pathname = "/me?application_sent"
                 });
             }
         }
